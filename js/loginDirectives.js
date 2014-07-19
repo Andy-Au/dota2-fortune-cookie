585422a176d3db24dd538c000744df5ac89e429d
Andy Au <au.andy.ch@gmail.com>
2014-07-19 01:44:50 -0400

    Folder structure reformat

--- js/loginDirectives.js
+++
@@ -1,8 +0,0 @@
-angular.module('loginDirectives', [])
-
-.directive('loginPicture', function () {
-	return {
-		restrict: 'E',
-    	template: "<img src='css/images/fortunecookie.jpg'</img>"
-  	};
-});
